28364a79020834f95df51d9e0dd0c15bd5acd50c
Maheen Ali <maheenali@Maheens-MacBook-Pro.local>
2023-12-11 10:53:10 -0600

    vari and functions

--- pwcrack.py
+++ pwcrack.py
@@ -1,5 +1,23 @@
+import sys
+import hashlib
+import bcrypt
+
+# Global variables to store hash type, crack method, and dictionary file
+hash_type = ""
+crack_method = ""
+dictionary_file = ""
 
 
-print ('hello')
+# Function to set the hash type
+def set_hash_type(ht):
+    global hash_type
+    hash_type = ht
 
+
+# Function to set the crack method
+def set_crack_method(cm):
+    global crack_method, dictionary_file
+    crack_method = cm
+    if cm == "Dictionary":
+        dictionary_file = ""
 
